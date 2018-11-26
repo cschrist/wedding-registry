a91a071169d2baf507803616b0697644e9e73808
Cam <csc98d@gmail.com>
2018-11-26 15:44:31 -0800

    Updates

---
+++ precache-manifest.86a7eb4960ecea7212a22e885d8de52b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "817b4e48ea35de01f5da",
+    "url": "/wedding-registry/static/js/main.817b4e48.chunk.js"
+  },
+  {
+    "revision": "e7810a2c2ca04667967d",
+    "url": "/wedding-registry/static/js/1.e7810a2c.chunk.js"
+  },
+  {
+    "revision": "483b618c66e5c05d9e71",
+    "url": "/wedding-registry/static/js/runtime~main.483b618c.js"
+  },
+  {
+    "revision": "56b1b77d27665b2d4cdae776c403568d",
+    "url": "/wedding-registry/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/wedding-registry/precache-manifest.86a7eb4960ecea7212a22e885d8de52b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/wedding-registry/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
